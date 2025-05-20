8191701a31a3e7540aba37e88b43f26d311c0e67
AllanMartinezC <all.martinezc@duocuc.cl>
2025-05-20 00:03:56 -0400

    Create GerenteRepository.java

---
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/repository/GerenteRepository.java
@@ -0,0 +1,8 @@
+package com.example.bilbioteca.duoc.BDD.repository;
+
+import com.example.bilbioteca.duoc.BDD.model.Gerente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GerenteRepository extends JpaRepository<Gerente, Long> {
+}
+
