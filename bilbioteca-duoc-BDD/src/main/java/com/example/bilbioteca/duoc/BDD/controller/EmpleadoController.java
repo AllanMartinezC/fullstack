531b5ac23292e652e376f33d899c180f528e70a5
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 23:34:36 -0400

    Update EmpleadoController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/EmpleadoController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/EmpleadoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bilbioteca.duoc.BDD.model.Empleado;
 import com.example.bilbioteca.duoc.BDD.services.EmpleadoService;
 
+@Tag(name = "Empleado", description = "Operaciones CRUD para Empleado")
 @RestController
 @RequestMapping("/empleados")
 public class EmpleadoController {
@@ -19,21 +20,26 @@ public class EmpleadoController {
     @Autowired
     private EmpleadoService empleadoService;
 
+    
+    @Operation(summary = "Obtener todos los empleados")
     @GetMapping
     public List<Empleado> listarTodos() {
         return empleadoService.obtenerTodos();
     }
-
+    
+    @Operation(summary = "Crear un nuevo Empleado")
     @PostMapping
     public Empleado crear(@RequestBody Empleado empleado) {
         return empleadoService.guardar(empleado);
     }
 
+    @Operation(summary = "Obtener empleado por ID")
     @GetMapping("/{id}")
     public Empleado obtenerPorId(@PathVariable Long id) {
         return empleadoService.obtenerPorId(id);
     }
 
+    @Operation(summary = "Eliminar empleado por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         empleadoService.eliminar(id);
