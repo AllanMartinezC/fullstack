dc1b0c041471be0562599989f0ea5413a27a1ca8
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 22:51:47 -0400

    Update VentaController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/VentaController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/VentaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bilbioteca.duoc.BDD.model.Venta;
 import com.example.bilbioteca.duoc.BDD.services.VentaService;
 
-
+@Tag(name = "Ventas", description = "Operaciones CRUD para las ventas")
 @RestController
 @RequestMapping("/ventas")
 public class VentaController {
@@ -20,21 +20,26 @@ public class VentaController {
     @Autowired
     private VentaService ventaService;
 
+    @Operation(summary = "Obtener todas las ventas")
     @GetMapping
     public List<Venta> obtenerVentas() {
         return ventaService.obtenerTodas();
     }
 
+    
+    @Operation(summary = "Obtener venta por ID")
     @GetMapping("/{id}")
     public Venta obtenerVentaPorId(@PathVariable Long id) {
         return ventaService.obtenerPorId(id);
     }
-
+    
+    @Operation(summary = "Guardar una venta")
     @PostMapping
     public Venta guardarVenta(@RequestBody Venta venta) {
         return ventaService.guardar(venta);
     }
 
+    @Operation(summary = "Borrar venta por ID")
     @DeleteMapping("/{id}")
     public void eliminarVenta(@PathVariable Long id) {
         ventaService.eliminar(id);
