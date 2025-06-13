3451a0e1b75b602e9af3a25a3e48eeee71cbc393
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 23:58:52 -0400

    Update LogisticaController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/LogisticaController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/LogisticaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/logistica")
 public class LogisticaController {
@@ -13,26 +15,31 @@ public class LogisticaController {
     @Autowired
     private LogisticaService logisticaService;
 
+    @Operation(summary = "Obtener todos los Empleados de Logistica")
     @GetMapping
     public List<Logistica> listar() {
         return logisticaService.obtenerTodos();
     }
 
+    @Operation(summary = "Obtener empleado Logistica por ID")
     @GetMapping("/{id}")
     public Logistica obtener(@PathVariable Long id) {
         return logisticaService.obtenerPorId(id);
     }
 
+    @Operation(summary = "Crear un nuevo Empleado de Logistica")
     @PostMapping
     public Logistica crear(@RequestBody Logistica logistica) {
         return logisticaService.guardar(logistica);
     }
 
+    @Operation(summary = "Actualizar Empleado Logistica por ID")
     @PutMapping("/{id}")
     public Logistica actualizar(@PathVariable Long id, @RequestBody Logistica nuevosDatos) {
         return logisticaService.actualizar(id, nuevosDatos);
     }
 
+    @Operation(summary = "Eliminar Empleado Logistica por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         logisticaService.eliminar(id);
