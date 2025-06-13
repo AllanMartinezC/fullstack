454a32a3b9a7156b587494110ad27aa0e390be4f
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 22:54:40 -0400

    Update UsuarioController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/UsuarioController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+@Tag(name = "Usuario", description = "Operaciones CRUD para el Usuario")
 @RestController
 @RequestMapping("/usuarios")
 
@@ -14,27 +16,32 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository repositorioUsuario;
 
-    
+    @Operation(summary = "Obtener todos los Usuarios")
     @GetMapping
     public List<Usuario> obtenerUsuarios() {
         return repositorioUsuario.findAll();
     }
-
+    
+    @Operation(summary = "Obtener Usuario por ID")
     @GetMapping("/{id}")
     public Usuario buscarPorId(@PathVariable Long id) {
         return repositorioUsuario.findById(id).orElse(null);
     }
 
+    @Operation(summary = "Guardar Usuario")
     @PostMapping
     public Usuario guardar(@RequestBody Usuario usuario) {
         return repositorioUsuario.save(usuario);
     }
-
+    
+    @Operation(summary = "Actualizar Usuario")
     @PutMapping
     public Usuario actualizar(@RequestBody Usuario usuario) {
         return repositorioUsuario.save(usuario);
     }
 
+    
+    @Operation(summary = "Eliminar Usuario por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         repositorioUsuario.deleteById(id);
