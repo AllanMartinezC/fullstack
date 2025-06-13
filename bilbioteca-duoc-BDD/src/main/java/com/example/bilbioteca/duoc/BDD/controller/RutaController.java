5632d1b28182f27f3514890faabd91cf04e5b865
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 22:57:41 -0400

    Update RutaController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/RutaController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/RutaController.java
@@ -7,33 +7,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Ruta", description = "Operaciones CRUD para las Rutas")
 @RestController
 @RequestMapping("/rutas")
 public class RutaController {
 
     @Autowired
     private RutaService rutaService;
+    
 
+    @Operation(summary = "Obtener todas las rutas")
     @GetMapping
     public List<Ruta> listar() {
         return rutaService.obtenerTodas();
     }
 
+    @Operation(summary = "Obtener ruta por ID")
     @GetMapping("/{id}")
     public Ruta obtener(@PathVariable Long id) {
         return rutaService.obtenerPorId(id);
     }
 
+    @Operation(summary = "Crear una nueva ruta")
     @PostMapping
     public Ruta crear(@RequestBody Ruta ruta) {
         return rutaService.guardar(ruta);
     }
 
+    @Operation(summary = "Actualizar una ruta por ID")
     @PutMapping("/{id}")
     public Ruta actualizar(@PathVariable Long id, @RequestBody Ruta nuevosDatos) {
         return rutaService.actualizar(id, nuevosDatos);
     }
 
+    @Operation(summary = "Eliminar una ruta por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         rutaService.eliminar(id);
