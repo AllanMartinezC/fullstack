69fde84996fc3f0604f57e43bc443f115a19e319
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 23:54:05 -0400

    Update ItemCarritoController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/ItemCarritoController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/ItemCarritoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Item Carrito", description = "Operaciones CRUD para Item Carrito")
 @RestController
 @RequestMapping("/item-carrito")
 public class ItemCarritoController {
@@ -14,21 +15,25 @@ public class ItemCarritoController {
     @Autowired
     private ItemCarritoService itemCarritoService;
 
+    @Operation(summary = "Guardar un Item en Item Carrito")
     @PostMapping("/guardar")
     public ItemCarrito guardarItem(@RequestBody ItemCarrito item) {
         return itemCarritoService.guardarItem(item);
     }
 
+    @Operation(summary = "Obtener todos los Items del Item Carrito")
     @GetMapping("/listar")
     public List<ItemCarrito> listarItems() {
         return itemCarritoService.listarItems();
     }
 
+    @Operation(summary = "Buscar un Item por ID")
     @GetMapping("/buscar/{id}")
     public ItemCarrito buscarPorId(@PathVariable Long id) {
         return itemCarritoService.buscarPorId(id);
     }
 
+    @Operation(summary = "Eliminar Item por ID")
     @DeleteMapping("/eliminar/{id}")
     public void eliminarItem(@PathVariable Long id) {
         itemCarritoService.eliminarItem(id);
