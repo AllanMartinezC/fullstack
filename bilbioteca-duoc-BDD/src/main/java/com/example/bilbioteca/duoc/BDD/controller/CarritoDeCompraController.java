1bdfcba44ba1e61cc9342436adc1d07630c42a2c
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-12 23:01:02 -0400

    Update CarritoDeCompraController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/CarritoDeCompraController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/CarritoDeCompraController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Carrito De Compra", description = "Operaciones CRUD para el Carrito de Compra")
 @RestController
 @RequestMapping("/carrito")
 public class CarritoDeCompraController {
@@ -14,21 +15,25 @@ public class CarritoDeCompraController {
     @Autowired
     private CarritoDeCompraService carritoDeCompraService;
 
+    @Operation(summary = "Crear un nuevo carrito de compra")
     @PostMapping
     public CarritoDeCompra crear(@RequestBody CarritoDeCompra carrito) {
         return carritoDeCompraService.guardarCarrito(carrito);
     }
 
+    @Operation(summary = "Obtener todos los carritos")
     @GetMapping
     public List<CarritoDeCompra> obtenerTodos() {
         return carritoDeCompraService.listarCarritos();
     }
 
+    @Operation(summary = "Obtener carrito por ID")
     @GetMapping("/{id}")
     public CarritoDeCompra obtenerPorId(@PathVariable Long id) {
         return carritoDeCompraService.buscarPorId(id);
     }
 
+    @Operation(summary = "Eliminar carrito por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         carritoDeCompraService.eliminarCarrito(id);
