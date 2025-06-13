fed99ab3680b10fdb6d6ed5eceb75f2c01c80607
AllanMartinezC <all.martinezc@duocuc.cl>
2025-06-13 00:01:48 -0400

    Update ProductoController.java

--- bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/ProductoController.java
+++ bilbioteca-duoc-BDD/src/main/java/com/example/bilbioteca/duoc/BDD/controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(name = "Producto", description = "Operaciones CRUD para Producto")
 @RestController
 @RequestMapping("/producto")
 
@@ -14,31 +15,37 @@ public class ProductoController {
     @Autowired
     private ProductoServices productoService;
 
+    @Operation(summary = "Obtener todos los Productos")
     @GetMapping
     public List<Producto> obtenerTodos() {
         return productoService.getProductos();
     }
 
+    @Operation(summary = "Obtener Producto por ID")
     @GetMapping("/{id}")
     public Producto obtenerPorId(@PathVariable Long id) {
         return productoService.getProductoById(id);
     }
 
+    @Operation(summary = "Agregar un nuevo Prodcuto")
     @PostMapping
     public Producto crear(@RequestBody Producto producto) {
         return productoService.saveProducto(producto);
     }
 
+    @Operation(summary = "Actualizar Producto por ID")
     @PutMapping("/{id}")
     public Producto actualizar(@PathVariable Long id, @RequestBody Producto nuevoProducto) {
         return productoService.actualizarProducto(id, nuevoProducto);
     }
 
+    @Operation(summary = "Eliminar Producto por ID")
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         productoService.deleteProducto(id);
     }
 
+    @Operation(summary = "Actualizar Producto en Stock")
     @PutMapping("/{id}/stock")
     public Producto ajustarStock(@PathVariable Long id, @RequestParam Long cantidad) {
         return productoService.ajustarStock(id, cantidad);
